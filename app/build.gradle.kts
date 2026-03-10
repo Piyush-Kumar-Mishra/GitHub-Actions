459a8a18282da1f78fc0898b09a3c63cd2cc84de
Piyush-Kumar-Mishra <piyushmishra.0220@gmail.com>
2026-03-10 23:09:09 +0530

    detekt setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("io.gitlab.arturbosch.detekt")
 }
 
 android {
@@ -58,4 +59,5 @@ dependencies {
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     testImplementation(kotlin("test"))
+    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.23.6")
 }
--- app/src/main/java/com/example/unittesting/Authentication.kt
+++ app/src/main/java/com/example/unittesting/Authentication.kt
@@ -1,5 +1,17 @@
 package com.example.unittesting
 
+/*
+ after adding plugin and dependencies run ./gradlew detekt (it will show error , which tells us it is working)
+ ( go to -> under project -> app> buid > reports > detekt.xml)
+  it is showing the error because the detekt is not followed from starting
+  to resolve the probelem we need baseline which will ignore the codes written previously before implementing detekt ./gradlew detektBaseline
+  then go to detekt-baseline.xml
+  now run the detekt , it will not show error
+*/
+
+
+
+
 class Authentication {
     val regex = "^[A-Za-z0-9+_.-]+@(.+)$".toRegex()
     fun signup(username : String,email: String, password: String) = when {
